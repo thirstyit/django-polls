3491c988074c7daab5345d2ed56d5d6b220fcc6e
Sam Ferguson <sam@thirstyit.com.au>
2023-12-17 21:10:01 +1100

    Updated views for future date errrors

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from .models import Question, Choice
 from django.urls import reverse
+from django.utils import timezone
 from django.views import generic
 # Create your views here.
 
@@ -12,7 +13,7 @@ class IndexView(generic.ListView):
     template_name = 'polls/index.html'
 
     def get_queryset(self):
-         return Question.objects.order_by('-pub_date')[:5]
+         return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Question
