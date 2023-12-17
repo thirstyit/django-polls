e9f2b323e1724e933c75b81865cfb14715979190
Sam Ferguson <sam@thirstyit.com.au>
2023-12-17 21:29:13 +1100

    Added new tests

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -3,8 +3,14 @@ import datetime
 from django.test import TestCase
 from django.utils import timezone
 from .models import Question
+from django.urls import reverse
+
 
 # Create your tests here.
+def create_question(question_text, days):
+    time = timezone.now() + datetime.delta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
+
 
 class QuestionModelTests(TestCase):
     
@@ -13,3 +19,27 @@ class QuestionModelTests(TestCase):
         future_question = Question(pub_date=time)
 
         self.assertIs(future_question.was_published_recently(), False)
+
+class QuestionIndexViewTests(TestCase):
+    def test_no_question(self):
+        response = self.client.get(reverse('polls:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+    def test_past_question(self):
+        create_question(question_text="Past question.", days=-30)
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            ['<Question: Past question.>']
+        )
+
+    def test_future_question(self):
+        create_question(question_text="Past question.", days=30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+
+
+
