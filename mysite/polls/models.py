f4e6848bd142789a87fc0f8955c06928595ecc16
Sam Ferguson <sam@thirstyit.com.au>
2023-12-25 10:55:44 +1100

    changed format of admin portal

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -15,6 +15,10 @@ class Question(models.Model):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
+    was_published_recently.short_description = "Published recently?"
+    was_published_recently.boolean = True
+    was_published_recently.admin_order_field = 'pub_date'
+
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
@@ -22,3 +26,4 @@ class Choice(models.Model):
     
     def __str__(self):
         return self.choice_text
+    
