b9996bbec8de15e9540fad452f98ee2bcd7c034a
Sam Ferguson <sam@thirstyit.com.au>
2023-12-25 11:35:49 +1100

    updated admin console header

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -76,9 +76,17 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+       'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': BASE_DIR / 'db.sqlite3',
     }
+    #'default': {
+     #   'ENGINE': 'django.db.backends.postgresql',
+      #  'NAME': 'polls',
+       # 'USER': keys.POSTGRES_USER,
+       # 'PASSWORD': keys.POSTGRES_PASSWORD,
+       # 'HOST': '127.0.0.1',
+       # 'PORT': '5432',
+   # }
 }
 
 
