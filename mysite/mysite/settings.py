a9566209276f4bcbede4c221198d5f7bafdfc534
Sam Ferguson <sam@thirstyit.com.au>
2024-01-01 15:44:22 +1100

    updated settings

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -9,9 +9,9 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-#import django_heroku
+import django_heroku
 import os
-from . import keys
+#from . import keys
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = keys.DJANGO_SECRET_KEY
-#SECRET_KEY = os.environ['SECRET_KEY']
+#SECRET_KEY = keys.DJANGO_SECRET_KEY
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-#DEBUG = os.environ.get('DEBUG', '') == 'True'
+#DEBUG = True
+DEBUG = os.environ.get('DEBUG', '') == 'True'
 
 ALLOWED_HOSTS = []
 
@@ -137,4 +137,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #Activate django_heroku
 
-#django_heroku.settings(locals())
+django_heroku.settings(locals())
